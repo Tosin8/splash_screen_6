e1fac0bf6703e8e1638a305212e15f3c104a346d
Tosin8 <tosinezekiel8@gmail.com>
2022-05-11 15:49:16 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# splash_screen_6
+
+Disclaimer: 
+
+This is my daily flutter practice code , an excerpt from flutter animation channel. 
---
+++ android/app/src/main/kotlin/com/example/splash_screen_6/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.splash_screen_6
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
